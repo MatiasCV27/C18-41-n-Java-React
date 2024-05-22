5cfd90bee53de3502bc416b55ed6c20b3969dfe7
tomasild <tomas@groovelist.co>
2024-05-21 22:41:50 -0400

    add Navbar component

---
+++ Client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+/* import { ChevronDown, Search, BellDot, Zap   } from 'lucide-react';
+import { Home, UserRound, Users, FolderHeart, CalendarCheck, BookmarkCheck, Globe  } from 'lucide-react';
+
+
+<ChevronDown />
+<Search />
+<BellDot />
+<Zap />
+
+<Home />
+<UserRound />
+<Users />
+<FolderHeart />
+<CalendarCheck />
+<BookmarkCheck />
+<Globe /> */
