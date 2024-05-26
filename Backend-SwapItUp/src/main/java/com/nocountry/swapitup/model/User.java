6927c8d725c3d05c4d6875f47855d95e80ae9a3a
Matias Criollo <matiascriollo57@gmail.com>
2024-05-26 17:23:19 -0500

    Arreglando el error

--- Backend-SwapItUp/src/main/java/com/nocountry/swapitup/model/User.java
+++ Backend-SwapItUp/src/main/java/com/nocountry/swapitup/model/User.java
@@ -78,11 +78,6 @@ public class User implements UserDetails {
     }
 
     @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
     public boolean isAccountNonExpired() {
         return true;
     }
