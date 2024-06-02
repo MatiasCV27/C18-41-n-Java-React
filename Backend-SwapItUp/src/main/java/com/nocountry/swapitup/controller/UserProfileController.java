008f8e0cd829d258a022921b318fdde528c37db8
Matias Criollo <matiascriollo57@gmail.com>
2024-06-02 17:27:51 -0500

    Haciendo merge con photo

--- Backend-SwapItUp/src/main/java/com/nocountry/swapitup/controller/UserProfileController.java
+++ Backend-SwapItUp/src/main/java/com/nocountry/swapitup/controller/UserProfileController.java
@@ -26,43 +26,34 @@ public class UserProfileController {
     private final UserProfileService userService;
 
     @GetMapping(value = "{username}")
-    @Operation(
-            summary = "Obtener usuario",
-            description = "Obtiene todos los datos del usuario que se ha hecho login a la pagina."
-    )
+    @Operation(summary = "Obtener usuario",
+            description = "Obtiene todos los datos del usuario que se ha hecho login a la pagina.")
     public ResponseEntity<UserProfileDto> getInfoUser(@PathVariable(value = "username") String username) {
         return ResponseEntity.ok(userService.getProfile(username));
     }
 
     @PutMapping(value = "{username}")
-    @Operation(
-            summary = "Editar usuario",
-            description = "Permite editar el nombre y el apellido del usuario logeado mediante su username."
-    )
+    @Operation(summary = "Editar usuario",
+            description = "Permite editar el nombre y el apellido del usuario logeado mediante su username.")
     public ResponseEntity<User> updateUserInfo(@PathVariable(value = "username") String username, @RequestBody User user) {
         return ResponseEntity.ok(userService.updateUserInfo(username, user));
     }
 
     @PutMapping(value = "/user/{id}")
-    @Operation(
-            summary = "Editar perfil",
-            description = "Permite editar la mayoria de los datos del perfil del usuario logeado mediante su Id."
-    )
+    @Operation(summary = "Editar perfil",
+            description = "Permite editar la mayoria de los datos del perfil del usuario logeado mediante su Id.")
     public ResponseEntity<Profile> updateProfileInfo(@PathVariable(value = "id") Integer id, @RequestBody Profile profile) {
         return ResponseEntity.ok(userService.updateProfileInfo(id, profile));
     }
 
     @DeleteMapping(value = "{username}")
-    @Operation(
-            summary = "Eliminar usuario",
-            description = "Permite eliminar el usuario junto a su perfil cuando e ingresa el nombre de dicho usuario"
-    )
+    @Operation(summary = "Eliminar usuario",
+            description = "Permite eliminar el usuario junto a su perfil cuando e ingresa el nombre de dicho usuario")
     public ResponseEntity<String> deleteUser(@PathVariable(value = "username") String username) {
         return ResponseEntity.ok(userService.eliminarUsuario(username));
     }
 
-    //IMAGEN
-
+    //todo: IMAGEN
     @PostMapping(value = "/{id}/image")
     public ResponseEntity<Profile> uploadImage(@PathVariable Integer id, @RequestParam MultipartFile imageFile){
         try{
