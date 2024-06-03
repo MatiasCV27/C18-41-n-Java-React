564151e55f9f82697b8a6bb48838f26ef71fc2a9
Jorge Villafraz <jorge9372@gmail.com>
2024-06-03 01:12:10 -0400

    Add edit profile form

--- Client/src/pages/configuracionPerfil/informacionPersonal/InformacionPersonal.container.tsx
+++ Client/src/pages/configuracionPerfil/informacionPersonal/InformacionPersonal.container.tsx
@@ -1,9 +1,33 @@
+import * as z from 'zod';
 import InformacionPersonalView from './InformacionPersonal.view';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
+const FormSchema = z.object({
+    name: z.string(),
+    lastname: z.string(),
+
+});
+type FormSchemaType = z.infer<typeof FormSchema>;
 export const InformacionPersonalContainer = () => {
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            
+            name: '',
+            lastname: '',
+        },
+    });
+
+    function onSubmit(values: FormSchemaType) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values);
+    }
+
     return (
         <>
-            <InformacionPersonalView />
+            <InformacionPersonalView form={form} onSubmit={onSubmit} />
         </>
     );
 };
--- Client/src/pages/configuracionPerfil/informacionPersonal/InformacionPersonal.view.tsx
+++ Client/src/pages/configuracionPerfil/informacionPersonal/InformacionPersonal.view.tsx
@@ -1,7 +1,100 @@
-const InformacionPersonalView = () => {
+import { FC, useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+interface Props {
+    form: any;
+    onSubmit: (data: { name: string; lastname: string }) => void;
+}
+
+const InformacionPersonalView: FC<Props> = ({ form, onSubmit }) => {
+    const [inputStates, setInputStates] = useState<{ [key: string]: boolean }>({
+        input1: false,
+        input2: false,
+        input3: false,
+    });
+
+    const handleToggleInput = (input: string) => {
+        setInputStates((prevInputStates) => ({
+            ...prevInputStates,
+            [input]: !prevInputStates[input],
+        }));
+    };
+
     return (
         <>
-            
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-8"
+                >
+                    <div className="flex justify-between items-center my-4">
+                        <div className="grid md:grid-cols-2 md:gap-x-2">
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <div className="relative ">
+                                            <div className="absolute -top-3.5 left-3">
+                                                <FormLabel>Nombre</FormLabel>
+                                            </div>
+
+                                            <FormControl>
+                                                <Input
+                                                    placeholder="Nombre"
+                                                    disabled={
+                                                        inputStates.input1
+                                                    }
+                                                    className="disabled:border-0 rounded border text-gray-700 border-gray-400 h-14 text-sm font-normal placeholder:text-gray-400"
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                        </div>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="lastname"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Apellido</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="shadcn"
+                                                disabled={inputStates.input1}
+                                                className="disabled:border-0 rounded border text-gray-700 border-gray-400 h-14 text-base font-normal placeholder:text-gray-400"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <Button
+                            type="button"
+                            variant={'ghost'}
+                            onClick={() => handleToggleInput('input1')}
+                        >
+                            Editar
+                        </Button>
+                    </div>
+                    <Button type="submit">Save</Button>
+                </form>
+            </Form>
         </>
     );
 };
