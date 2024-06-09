25a46e739a58490e86d878b2f3236ebdc9804fcd
tomasild <tomas@groovelist.co>
2024-06-09 12:41:51 -0400

    add Carpeta icons y Cards tutores

---
+++ Client/src/components/TutoresHome/TutoresHome.container.tsx
@@ -0,0 +1,11 @@
+import TutoresHomeView from "./TutoresHome.view";
+
+const TutoresHomeContainer: React.FC = () => {
+  return (
+    <>
+      <TutoresHomeView />
+    </>
+  );
+};
+
+export default TutoresHomeContainer;
